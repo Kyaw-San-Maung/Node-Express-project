cdd78f2b79e46334cdc64d264fdc0f943020aba2
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-06-24 01:26:29 +0630

    feat: add middleware

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,42 +1,49 @@
+const asynHandler = require("express-async-handler");
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
-const getAllContacts = (req, res) => {
+const getAllContacts = asynHandler(async (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Get contact
 //@route GET /api/contacts
 //@access public
 
-const getContact = (req, res) => {
+const getContact = asynHandler(async (req, res) => {
   res.status(200).json({ message: `Get contact for ${req.params.id}` });
-};
+});
 
 //@desc Create new contacts
 //@route POST /api/contacts
 //@access public
 
-const createContacts = (req, res) => {
+const createContacts = asynHandler(async (req, res) => {
+  console.log("The request body data is : ", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
   res.status(201).json({ message: "Create Contact" });
-};
+});
 
 //@desc Update Contact
 //@route POST /api/contacts
 //@access public
 
-const updateContacts = (req, res) => {
+const updateContacts = asynHandler(async (req, res) => {
   res.status(200).json({ message: `Update contact for ${req.params.id}` });
-};
+});
 
-//@desc Update Contact
-//@route POST /api/contacts
+//@desc Delete Contact
+//@route DELETE /api/contacts
 //@access public
 
-const deleteContacts = (req, res) => {
+const deleteContacts = asynHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contact for ${req.params.id}` });
-};
+});
 module.exports = {
   getAllContacts,
   createContacts,
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -8,14 +8,8 @@ const {
 } = require("../controllers/contactController");
 const router = express.Router();
 
-router.route("/").get(getAllContacts);
+router.route("/").get(getAllContacts).post(createContacts);
 
-router.route("/").post(createContacts);
-
-router.route("/:id").get(getContact);
-
-router.route("/:id").put(updateContacts);
-
-router.route("/:id").delete(deleteContacts);
+router.route("/:id").get(getContact).put(updateContacts).delete(deleteContacts);
 
 module.exports = router;
