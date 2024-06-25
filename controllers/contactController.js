56ae4eaca46d0dc0d89a0bb02987caf9fe449d0d
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-06-25 22:23:54 +0630

    feat: add curd operation

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,10 +1,12 @@
 const asynHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
 const getAllContacts = asynHandler(async (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
 });
 
 //@desc Get contact
@@ -12,7 +14,12 @@ const getAllContacts = asynHandler(async (req, res) => {
 //@access public
 
 const getContact = asynHandler(async (req, res) => {
-  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  res.status(200).json(contact);
 });
 
 //@desc Create new contacts
@@ -26,7 +33,12 @@ const createContacts = asynHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields are mandatory!");
   }
-  res.status(201).json({ message: "Create Contact" });
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
 });
 
 //@desc Update Contact
